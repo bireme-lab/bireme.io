e0a925b04299fdaca1899291b5a5d33e0c86fdc7
Antoine Lin <antoine@sidevision.io>
2024-03-05 19:00:25 +0100

    fix rss

--- app/[locale]/rss.xml/route.ts
+++ app/[locale]/rss.xml/route.ts
@@ -2,6 +2,7 @@ import { getMeta } from "@/content/meta";
 import { i18n } from "@/utils/i18n";
 import * as MDX from "@/utils/mdx";
 import { Option } from "@swan-io/boxed";
+import { getTranslations } from "next-intl/server";
 import RSS from "rss";
 import { match, P } from "ts-pattern";
 
@@ -9,12 +10,15 @@ export const dynamic = "force-dynamic"; // defaults to auto
 
 export async function GET(request: Request) {
   const url = new URL(request.url);
-  const locale = match(url)
-    .with({ pathname: P.string.includes("/fr/") }, () => i18n.locales["0"])
-    .with({ pathname: P.string.includes("/en/") }, () => i18n.locales["1"])
+
+  const locale = match(url.searchParams.get("locale"))
+    .with(P.string.regex("fr"), () => i18n.locales["0"])
+    .with(P.string.regex("en"), () => i18n.locales["1"])
     .otherwise(() => i18n.defaultLocale);
 
   const meta = await getMeta(locale);
+
+  const t = await getTranslations({ locale, namespace: "route.rss" });
 
   const feed = new RSS({
     title: meta.title as string,
@@ -35,8 +39,8 @@ export async function GET(request: Request) {
       posts.map((post) => {
         feed.item({
           title: post.title,
-          description: post.body,
-          url: `${url.origin}/${locale}/${post.slug}`,
+          description: `<p>tl;dr</br></br>${post.tldr}</p></br><a href="${url.origin}/${locale}/blog/${post.slug}" title="${t("read_more")}">${t("read_more")}</a>`,
+          url: `${url.origin}/${locale}/blog/${post.slug}`,
           guid: post.slug,
           date: post.publishedAt,
           author: "Bireme Lab",
