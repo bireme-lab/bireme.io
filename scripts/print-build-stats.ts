9ecace1c95a7c700063f4b6603e56428bfaa97c0
Antoine Lin <antoine@sidevision.io>
2024-06-11 17:44:26 +0200

    rename VERCEL_URL to DEPLOYMENT_URL

--- scripts/print-build-stats.ts
+++ scripts/print-build-stats.ts
@@ -1,10 +1,10 @@
 /* eslint-disable no-console */
-import { IS_DEV, ORIGIN, PROTOCOL, VERCEL_URL } from "../utils/vars";
+import { DEPLOYMENT_URL, IS_DEV, ORIGIN, PROTOCOL } from "../utils/vars";
 
 const main = () => {
   console.log("\n*************************\n");
   console.log(`IS_DEV: ${IS_DEV}`);
-  console.log(`VERCEL_URL: ${VERCEL_URL}`);
+  console.log(`DEPLOYMENT_URL: ${DEPLOYMENT_URL}`);
   console.log(`ORIGIN: ${ORIGIN}`);
   console.log(`PROTOCOL: ${PROTOCOL}`);
   console.log("\n*************************\n");
