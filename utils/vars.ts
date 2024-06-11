70b13f4811c4639195749aa5b0e01b9abbd7cd2f
Antoine Lin <antoine@sidevision.io>
2024-06-11 17:44:26 +0200

    rename VERCEL_URL to DEPLOYMENT_URL

--- utils/vars.ts
+++ utils/vars.ts
@@ -2,8 +2,8 @@ import { envs } from "../publicEnvs";
 
 export const IS_DEV = envs.IS_DEV === "true";
 export const PROTOCOL = IS_DEV ? "http" : "https";
-export const VERCEL_URL =
+export const DEPLOYMENT_URL =
   envs.NEXT_PUBLIC_VERCEL_ENV === "production"
     ? envs.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL
     : envs.NEXT_PUBLIC_VERCEL_URL;
-export const ORIGIN = `${PROTOCOL}://${VERCEL_URL ?? "localhost"}${IS_DEV ? ":3000" : ""}`;
+export const ORIGIN = `${PROTOCOL}://${DEPLOYMENT_URL ?? "localhost"}${IS_DEV ? ":3000" : ""}`;
